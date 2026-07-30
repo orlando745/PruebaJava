b11b55d96979afd161ae9dd4f1f5f72c5e4f3ebf
Juan Perez <jperez@gmail.com>
2026-07-29 21:53:57 -0400

    Creacion del main principal

---
+++ src/Ejercicio1/Principal.java
@@ -0,0 +1,10 @@
+package Ejercicio1;
+public class Principal {
+    public static void main(String[] args) {
+        Operaciones op= new Operaciones();
+        System.out.println("Suma de dos numeros "+op.suma(5, 6));
+        System.out.println("Resta de dos numeros "+op.suma(5, 6));
+        System.out.println("Multiplicacion de dos numeros "+op.suma(5, 6));
+        System.out.println("Division de dos numeros "+op.suma(8, 4));
+    }
+}
