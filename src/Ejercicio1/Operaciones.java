2d833d9859317b4698b9661cf5946828b3459946
Juan Perez <jperez@gmail.com>
2026-07-29 21:43:54 -0400

    Creacion de la clase operaciones

---
+++ src/Ejercicio1/Operaciones.java
@@ -0,0 +1,15 @@
+package Ejercicio1;
+public class Operaciones {
+    public int suma(int a, int b){
+        return a+b;
+    }
+    public int resta(int a, int b){
+        return a-b;
+    }
+    public int multiplicacion(int a, int b){
+        return a*b;
+    }
+    public int division(int a, int b){
+        return a/b;
+    }
+}
