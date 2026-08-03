eb76ecb69157f8917e7e68e81a066cb3f978d581
Juan Perez <jperez@gmail.com>
2026-08-02 20:44:42 -0400

    Creacione de metodos minimo y maximo

--- src/Ejercicio1/Operaciones.java
+++ src/Ejercicio1/Operaciones.java
@@ -12,4 +12,10 @@ public class Operaciones {
     public int division(int a, int b){
         return a/b;
     }
+    public int maximo(int a, int b){
+        return Math.max(a, b);
+    }
+    public int minimo(int a, int b){
+        return Math.min(a, b);
+    }
 }
